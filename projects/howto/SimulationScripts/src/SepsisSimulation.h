43fd4b46b74f57eef5ffc6b1a10621b31843cfd7
faaiz <faaiztaufiq@gmail.com>
2021-02-18 16:18:11 +0000

    Functioning mimic simulator

---
+++ projects/howto/SimulationScripts/src/SepsisSimulation.h
@@ -0,0 +1,179 @@
+/**************************************************************************************
+Copyright 2015 Applied Research Associates, Inc.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the License
+at:
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+**************************************************************************************/
+
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include <utility> // std::pair
+#include <stdexcept> // std::runtime_error
+#include <sstream>
+#include <CsvUtils.h>
+#include <PatientUtils.h>
+#include <stdio.h>
+#include <time.h>
+#include <biogears/cdm/patient/actions/SEConsumeNutrients.h>
+#include <biogears/cdm/system/physiology/SEGastrointestinalSystem.h>
+#include <biogears/cdm/system/physiology/SECardiovascularSystem.h>
+#include <biogears/cdm/system/physiology/SEEnergySystem.h>
+#include <biogears/cdm/patient/SENutrition.h>
+#include <biogears/cdm/patient/SEPatient.h>
+#include <biogears/cdm/properties/SEScalarTypes.h>
+#include <biogears/cdm/engine/PhysiologyEngineTrack.h>
+#include <biogears/cdm/compartment/SECompartmentManager.h>
+#include <biogears/engine/BioGearsPhysiologyEngine.h>
+#include <biogears/string/manipulation.h>
+
+#include <biogears/cdm/patient/actions/SEInfection.h>
+#include <biogears/cdm/patient/actions/SESubstanceCompoundInfusion.h>
+#include <biogears/cdm/patient/actions/SESubstanceInfusion.h>
+#include <biogears/cdm/substance/SESubstance.h>
+#include <biogears/cdm/substance/SESubstanceCompound.h>
+#include <biogears/cdm/substance/SESubstanceManager.h>
+#include <map>
+#include <mutex>
+#include <thread>
+#include <future>
+
+namespace biogears {
+class SEInfection;
+class SEInflammatoryResponse;
+class SESubstance;
+class SESubstanceInfusion;
+class SESubstanceCompoundInfusion;
+}
+
+class SepsisSimulation {
+
+private:
+	std::mutex mutex;
+	std::unique_ptr<biogears::PhysiologyEngine> bg;
+    std::map<std::string, std::vector<std::string>> mimic_data;
+    const std::string currentDateTime() {
+        time_t     now = time(0);
+        struct tm  tstruct;
+        char       buf[80];
+        tstruct = *localtime(&now);
+        strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+        return buf;
+    }
+
+public:
+	SepsisSimulation(std::string mimicdir, double icustayid, std::string exportdir)
+	{
+        std::string time_now = currentDateTime();
+		bg = CreateBioGearsEngine("SimulateMIMIC_" + std::to_string(icustayid) + "_" + time_now + ".log");
+		bg->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("HeartRate", FrequencyUnit::Per_min);
+		bg->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("BloodVolume", VolumeUnit::mL);
+		bg->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("CardiacOutput", VolumePerTimeUnit::mL_Per_min);
+		bg->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("MeanArterialPressure", PressureUnit::mmHg);
+		bg->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("SystolicArterialPressure", PressureUnit::mmHg);
+		bg->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("DiastolicArterialPressure", PressureUnit::mmHg);
+		bg->GetEngineTrack()->GetDataRequestManager().CreatePhysiologyDataRequest().Set("StomachContents-Water",VolumeUnit::mL);
+			
+		bg->GetEngineTrack()->GetDataRequestManager().SetResultsFilename(exportdir + "/SimulateMIMIC_" + std::to_string(icustayid) + "_" + time_now + ".csv");
+        SEPatient pt(bg->GetLogger(), true);
+		CsvUtils csvUtils(mimicdir, icustayid);
+        mimic_data = csvUtils.get_data();
+		
+		PatientUtils::create_patient(&pt, &csvUtils, icustayid);
+		
+		if (!bg->InitializeEngine(pt)) {
+			bg->GetLogger()->Error("Could not load state, check the error");
+			return;
+		}
+	}
+
+	void advance_time(int time, TimeUnit unit)
+	{
+        std::lock_guard<std::mutex> l(mutex);
+        bg->GetLogger()->Info("Starting to advance time...............................");
+		bg->AdvanceModelTime(time, unit);
+        bg->GetLogger()->Info("Advanced time by " + std::to_string(time) + unit.GetString());
+	}
+
+    void sepsis_infection(double sofa)
+    {
+        std::lock_guard<std::mutex> l(mutex);
+        double sofa_frac = sofa/24;
+        CDM::enumInfectionSeverity::value severity;
+        if (sofa_frac < 1.0/3) {
+            severity = CDM::enumInfectionSeverity::Mild;
+        } else if (sofa_frac < 2.0/3) {
+            severity = CDM::enumInfectionSeverity::Moderate;
+        } else {
+            severity = CDM::enumInfectionSeverity::Severe;
+        }
+        SEInfection infection {};
+        infection.SetSeverity(severity);
+        SEScalarMassPerVolume infection_mic;
+        infection_mic.SetValue(8.0, MassPerVolumeUnit::g_Per_L);
+        infection.GetMinimumInhibitoryConcentration().Set(infection_mic);
+        infection.SetLocation("Gut");
+        bg->GetLogger()->Info("Initiating infection with severity: " + std::to_string(sofa_frac));
+        bg->ProcessAction(infection);
+    }
+
+	void administer_vasopressors(double rate) {
+        std::lock_guard<std::mutex> l(mutex);
+        double weight = bg->GetPatient().GetWeight(MassUnit::kg);
+		SESubstance* norepinephrine = bg->GetSubstanceManager().GetSubstance("Norepinephrine");
+		SESubstanceInfusion m_pressor {*norepinephrine};
+		m_pressor.GetRate().SetValue(rate*weight, VolumePerTimeUnit::mL_Per_min);
+		m_pressor.GetConcentration().SetValue(1.0, MassPerVolumeUnit::ug_Per_mL);
+        bg->GetLogger()->Info("Starting to administer vasopressors...........................");
+		bg->ProcessAction(m_pressor);
+        bg->GetLogger()->Info("Administered vasopressors......................................");
+	}
+
+    void administer_iv(double dose, bool preadm=false)
+    {
+        std::lock_guard<std::mutex> l(mutex);
+        SESubstanceCompound* saline = bg->GetSubstanceManager().GetCompound("Saline");
+        SESubstanceCompoundInfusion m_saline {*saline};
+        m_saline.GetBagVolume().SetValue(dose, VolumeUnit::mL);
+        if (preadm) {
+            m_saline.GetRate().SetValue(750.0/60, VolumePerTimeUnit::mL_Per_min);
+        } else {
+            m_saline.GetRate().SetValue(dose/60, VolumePerTimeUnit::mL_Per_min);
+        }
+        bg->GetLogger()->Info("Starting to administer IV fluids.....................");
+        bg->ProcessAction(m_saline);
+        bg->GetLogger()->Info("Administered IV fluids................................");
+    }
+
+    void consume_nutrients(double time_step)
+    {
+        std::lock_guard<std::mutex> l(mutex);
+        SEConsumeNutrients nutrients;
+        if (((int) time_step) % 4*3600) {
+            bg->GetLogger()->Info("Patient eats food.....................");
+            nutrients.GetNutrition().GetCarbohydrate().SetValue(130, MassUnit::g);
+            nutrients.GetNutrition().GetFat().SetValue(27, MassUnit::g);
+            nutrients.GetNutrition().GetProtein().SetValue(20, MassUnit::g);
+            nutrients.GetNutrition().GetCalcium().SetValue(5000, MassUnit::mg);
+            nutrients.GetNutrition().GetSodium().SetValue(0.75, MassUnit::g);
+            nutrients.GetNutrition().GetWater().SetValue(300, VolumeUnit::mL);            
+        } else {
+            bg->GetLogger()->Info("Patient drinks water.....................");
+            nutrients.GetNutrition().GetWater().SetValue(150, VolumeUnit::mL);
+        }
+        bg->ProcessAction(nutrients);
+    }
+
+    std::map<std::string, std::vector<std::string>> get_mimic_data() const
+    {
+        return mimic_data;
+    }
+
+};
---
+++ projects/howto/SimulationScripts/src/SepsisSimulation_main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <typeinfo>
+#include <stdio.h>
+#include <SepsisSimulation.h>
+
+void simulate_mimic(const std::string& mimicdir, double icustayid, const std::string& exportdir)
+{
+    SepsisSimulation sepsisSimulation(mimicdir, icustayid, exportdir);
+    double time_step = 0;
+    std::map<std::string, std::vector<std::string>> data = sepsisSimulation.get_mimic_data();
+    sepsisSimulation.sepsis_infection(std::stod(data["SOFA"].at(0)));
+    double preadm_input = std::stod(data["input_total"].at(0)) - std::stod(data["input_1hourly"].at(0));
+    sepsisSimulation.administer_iv(preadm_input, true);
+    for (int i = 0; i < std::ceil(preadm_input/750); i++) {
+        sepsisSimulation.consume_nutrients(time_step);
+        sepsisSimulation.advance_time(3600, TimeUnit::s);
+        time_step += 3600;
+    }
+    double prev_time_step = std::stod(data["charttime"].at(0));
+    int rowNum = data["charttime"].size();
+    for(int i = 0; i < rowNum; i++) {
+        double time_step_jump = std::stod(data["charttime"].at(i)) - prev_time_step;
+        int t = 0;
+        while (t < ((int)time_step_jump/3600)) {
+            sepsisSimulation.consume_nutrients(time_step);
+            sepsisSimulation.advance_time(3600, TimeUnit::s);
+            time_step += 3600;
+            t += 1;
+        }
+        sepsisSimulation.advance_time(time_step_jump - t*3600, TimeUnit::s);
+        double iv_input = std::stod(data["input_1hourly"].at(i));
+        sepsisSimulation.administer_iv(iv_input);
+        double vp_rate = std::stod(data["median_dose_vaso"].at(i));
+        sepsisSimulation.administer_vasopressors(vp_rate);
+        prev_time_step = std::stod(data["charttime"].at(i));
+    }
+}
+
+int main( int argc, char* argv[] )
+{
+  simulate_mimic(argv[1], std::stod(argv[2]), argv[3]);
+  return 0;
+}
